03a4fa0351293de26c2c0d099f2535a1a1a49d86
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-08-10 00:22:34 +0100

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ dotenv.config();
 
 const {
   OPENAI_API_KEY,
-  R2_ACCESS_KEY_ID,
+  R2_ACCESS_KEY,
   R2_SECRET_KEY,
   R2_BUCKET_NAME,
   R2_ENDPOINT,
@@ -43,7 +43,7 @@ const s3 = new S3Client({
   region: "auto",
   endpoint: R2_ENDPOINT,
   credentials: {
-    accessKeyId: R2_ACCESS_KEY_ID || "",
+    accessKeyId: R2_ACCESS_KEY || "",
     secretAccessKey: R2_SECRET_KEY || ""
   },
   forcePathStyle: false // R2 works with virtual-host style
